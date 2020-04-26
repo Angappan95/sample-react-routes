5de289edefc8e01d251617465d204ae1b7b9b915
AG <angappan.ganesan@freshworks.com>
2020-04-26 23:10:06 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, Link, Switch, BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Contact from './components/Contact'
+import User from './components/User'
+import Product from './components/Product'
+import NotFound from './components/NotFound'
+
+const routing = (
+    
+    <Router>
+      <div className='navBar'>
+        <Link to='/'>Home</Link><br/>
+        <Link to='/product'>Product</Link><br/>
+        <Link to='/user'>User</Link><br/>
+        <Link to='/contact'>Contact</Link><br/>
+      </div>
+      <Switch>
+        <Route exact path='/' component={App}></Route>
+        <Route path='/contact' component={Contact}></Route>
+        <Route path='/user' component={User}></Route>
+        <Route path='/product' component={Product}></Route>
+        <Route component={NotFound}></Route>
+      </Switch>
+    </Router>
+    )
+    
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
